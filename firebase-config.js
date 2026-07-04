03e47762dbceaaf2f04c410b682b81f92d97602d
mainism <mainulislam.ruet20@gmail.com>
2026-07-05 01:44:47 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,10 @@
-// DayFlow Firebase config
-// 1) Create a Firebase web app in the Firebase console.
-// 2) Copy the firebaseConfig object.
-// 3) Replace the placeholder values below.
-// 4) Keep this file named firebase-config.js and upload it with the rest of the app.
-
-window.DAYFLOW_FIREBASE_CONFIG = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAW6btiXT7Grop-4oFAH1Wtc5ipdhdXAfk",
   authDomain: "track-it-9ce57.firebaseapp.com",
   projectId: "track-it-9ce57",
   storageBucket: "track-it-9ce57.firebasestorage.app",
   messagingSenderId: "322177819287",
-  appId: "1:322177819287:web:7549e776fe00b4c52b3fd2"
+  appId: "1:322177819287:web:7549e776fe00b4c52b3fd2",
+  measurementId: "G-E0821SG7MQ"
 };
