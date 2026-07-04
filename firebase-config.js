8090d5717be47dd770528aa38b6a65cacf6e09b3
mainism <mainulislam.ruet20@gmail.com>
2026-07-05 01:41:58 +0600

    Fix formatting in firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,11 +5,10 @@
 // 4) Keep this file named firebase-config.js and upload it with the rest of the app.
 
 window.DAYFLOW_FIREBASE_CONFIG = {
-apiKey: "AIzaSyAW6btiXT7Grop-4oFAH1Wtc5ipdhdXAfk",
+  apiKey: "AIzaSyAW6btiXT7Grop-4oFAH1Wtc5ipdhdXAfk",
   authDomain: "track-it-9ce57.firebaseapp.com",
   projectId: "track-it-9ce57",
   storageBucket: "track-it-9ce57.firebasestorage.app",
   messagingSenderId: "322177819287",
-  appId: "1:322177819287:web:7549e776fe00b4c52b3fd2",
-  measurementId: "G-E0821SG7MQ"
+  appId: "1:322177819287:web:7549e776fe00b4c52b3fd2"
 };
