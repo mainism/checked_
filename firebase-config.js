a6334b068e4f9e4306602c74a70527fb09ad6746
mainism <mainulislam.ruet20@gmail.com>
2026-07-05 01:34:05 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// DayFlow Firebase config
+// 1) Create a Firebase web app in the Firebase console.
+// 2) Copy the firebaseConfig object.
+// 3) Replace the placeholder values below.
+// 4) Keep this file named firebase-config.js and upload it with the rest of the app.
+
+window.DAYFLOW_FIREBASE_CONFIG = {
+  apiKey: "PASTE_API_KEY_HERE",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT_ID.appspot.com",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
